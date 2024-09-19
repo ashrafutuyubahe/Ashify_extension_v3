fdd8b55533ac2268919287b3562a29149155bc69
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-19 07:56:40 +0200

    fixed the bug of running the extension

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 // This method is called when your extension is activated
@@ -10,18 +8,12 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ashify" is now active!');
+	
+	console.log('welcome  to ashify , relax by listening music and streaming without leaving vscode!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ashify.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Ashify!');
+	const disposable = vscode.commands.registerCommand('ashify.greetingsFromAshify', function () {
+		
+		vscode.window.showInformationMessage('welcome  to ashify , relax by listening music and streaming without leaving vscode!');
 	});
 
 	context.subscriptions.push(disposable);
