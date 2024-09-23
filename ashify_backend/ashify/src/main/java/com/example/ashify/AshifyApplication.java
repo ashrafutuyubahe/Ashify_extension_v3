14ad253c77993cdc50bc0eded5d7be63c2c90c6f
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-23 08:49:19 +0200

    configuring the  java backend

--- ashify_backend/ashify/src/main/java/com/example/ashify/AshifyApplication.java
+++ ashify_backend/ashify/src/main/java/com/example/ashify/AshifyApplication.java
@@ -8,6 +8,12 @@ public class AshifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AshifyApplication.class, args);
+		getMovies();
+	
+	}
+
+	public static void getMovies(){
+		System.out.println("heee is  a  list of movies");
 	}
 
 }
