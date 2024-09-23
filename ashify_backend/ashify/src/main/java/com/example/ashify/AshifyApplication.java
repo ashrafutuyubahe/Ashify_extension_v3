5a5becebdbeabe7b04811f4b6f53f8e731bbaec3
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-23 08:41:26 +0200

    setting working  environment

---
+++ ashify_backend/ashify/src/main/java/com/example/ashify/AshifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ashify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshifyApplication.class, args);
+	}
+
+}
