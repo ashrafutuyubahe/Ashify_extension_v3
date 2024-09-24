7a39e03dc94ac40be9a6d202d35a04e3a9e0a2df
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-24 07:36:32 +0200

    setting backend folder structure

---
+++ ashify_backend/ashify/Ashify/src/main/java/com/Ashify_extension/Ashify/AshifyApplication.java
@@ -0,0 +1,13 @@
+package com.Ashify_extension.Ashify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshifyApplication.class, args);
+	}
+
+}
---
+++ ashify_backend/ashify/Ashify/src/test/java/com/Ashify_extension/Ashify/AshifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ashify_extension.Ashify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
