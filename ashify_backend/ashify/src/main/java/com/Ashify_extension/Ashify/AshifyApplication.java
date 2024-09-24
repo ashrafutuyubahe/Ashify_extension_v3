1ff9b53fddf388b0ea6aa1a4870396c43434752d
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-24 09:59:04 +0200

    code refactoring

--- ashify_backend/ashify/src/main/java/com/Ashify_extension/Ashify/AshifyApplication.java
+++ ashify_backend/ashify/src/main/java/com/Ashify_extension/Ashify/AshifyApplication.java
@@ -1,14 +1,8 @@
 package com.Ashify_extension.Ashify;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 
