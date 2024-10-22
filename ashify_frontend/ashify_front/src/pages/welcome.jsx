8b89fd3da5fe930e6af250faefb41eef466c50d6
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-22 17:57:35 +0200

    finished the welcome scren

--- ashify_frontend/ashify_front/src/pages/welcome.jsx
+++ ashify_frontend/ashify_front/src/pages/welcome.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { FaSpotify } from 'react-icons/fa';
-import "../styles/welcome.css"
+import "../styles/welcome.css";
 
 const WelcomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-white">
-      <h2 className="text-4xl text-white  mb-9">Welcome to Ashify</h2>
-      <button className='bg-green-500 text-white p-4 rounded-full hover:bg-green-600 '>
-        <FaSpotify className="text-6xl" />
+    <div className="welcome-container flex flex-col items-center justify-center h-screen bg-[#1E1E1E]">
+      <h2 className="welcome-heading text-5xl font-bold ">Welcome to Ashify</h2>
+      <button onClick={handleContinueWithSpotify} className="spotify-button bg-green-500 p-6 rounded-full hover:bg-green-600 transition duration-300 ease-in-out">
+        <FaSpotify className="spotify-icon text-white text-7xl" />
+        <h4 className='icon_text'>continue with Spotify</h4>
       </button>
     </div>
   );
 };
 
+const handleContinueWithSpotify= ()=>{
+    alert("you clicked")
+}
 export default WelcomePage;
