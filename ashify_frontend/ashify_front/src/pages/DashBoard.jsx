37f7fa02918067df230a779e4f4d9a98955546b0
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-23 06:23:54 +0200

    working on playing music

--- ashify_frontend/ashify_front/src/pages/DashBoard.jsx
+++ ashify_frontend/ashify_front/src/pages/DashBoard.jsx
@@ -21,72 +21,77 @@ const DashBoard = () => {
         </div>
         <div className="DashBoard_Section_container">
           <div className="playList_section">
-
-          <div className="playlistActions">
-          <h2 id="playlist_text"> Your playlists</h2>
-          <FaSearch className="search_icon_playlist " />
-           <span id="add_playlist_button">+</span>
-
-          </div>
-          <div className="playlists">
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
+            <div className="playlistActions">
+              <div className="sections">
+                <div className="first_playlist_section">
+                  <h2 id="playlist_text"> Your playlists</h2>
+                  <FaSearch
+                    onClick={handle_playlist_search}
+                    className="search_icon_playlist "
+                  />
+                </div>
+                <div className="second_playlist_section">
+                  <span id="add_playlist_button">+</span>
+                </div>
               </div>
             </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
+            <div className="playlists">
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
+              </div>
+              <div className="user_playlist">
+                <img className="card_image" src={card_image} alt="image_card" />
+                <div className="playlist_details">
+                  <h4 className="liked_songs_text">Liked Songs</h4>
+                  <h4 className="playlist_songs">play list .8 songs</h4>
+                </div>
               </div>
             </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
-              </div>
-            </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
-              </div>
-            </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
-              </div>
-            </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
-              </div>
-            </div>
-            <div className="user_playlist">
-             <img  className="card_image" src={card_image} alt="image_card" />
-              <div className="playlist_details">
-              <h4 className="liked_songs_text">Liked Songs</h4>
-              <h4 className="playlist_songs">play list .8 songs</h4>
-              </div>
-            </div>
-            
-           
-            </div>  
           </div>
           <div className="popular_music_section">
             <h3 id="popula_music_text">Popular Music</h3>
             <div className="popular_music_container">
               <div className="card_container">
-                <div className="music_card"></div>
+                <div onClick={handle_play_music} className="music_card"></div>
                 <h5 className="artist_name">HEIS Rema</h5>
               </div>
               <div className="card_container">
@@ -121,4 +126,14 @@ const DashBoard = () => {
   );
 };
 
+const handle_play_music = () => {
+  alert("u wanna play music");
+return (
+    <Display_music/>
+)
+};
+
+const handle_playlist_search = () => {
+  alert("you wanna search playlist");
+};
 export default DashBoard;
