95a941926c1f637b3ddd8782db175e5dc1f593cb
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-23 11:18:36 +0200

    making c+ and c programms

---
+++ ashify_frontend/ashify_front/adding.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b,sum;
+
+    cout << "please enter  2 numbers: " <<endl;
+    cin >> a >> b;
+    sum= a+b;
+
+    cout << "the sum is " << sum;
+}
