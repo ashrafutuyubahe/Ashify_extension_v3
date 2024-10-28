aeb0b5389bd2fc0392cd3ff36b5559c5f9feb411
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-28 05:50:06 +0200

    established the connection

--- ashify_backend/ashify/src/main/java/com/Ashify_extension/Ashify/service/MakeUserLogin.java
+++ ashify_backend/ashify/src/main/java/com/Ashify_extension/Ashify/service/MakeUserLogin.java
@@ -2,16 +2,12 @@ package com.Ashify_extension.Ashify.service;
 
 import org.springframework.stereotype.Service;
 
-import com.Ashify_extension.Ashify.Entity.User;
+
 
 @Service
 public class MakeUserLogin {
 
     public void  handleUserLogin(){
-        var user= new User();
-        String userEmail= user.getUserEmail();
-        String userName=user.getUserName();
-
-
+       System.out.println("u are about  to login");
     }
 }
